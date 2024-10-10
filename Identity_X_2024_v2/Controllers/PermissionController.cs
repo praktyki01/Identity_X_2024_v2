aa8d7bea008707e1db5dbee5a366e97ab64721ca
ETI <ETI@DESKTOP-BEI752H>
2024-10-10 10:44:22 +0200

    wykomentowanie metod z PermissionController

--- Identity_X_2024_v2/Controllers/PermissionController.cs
+++ Identity_X_2024_v2/Controllers/PermissionController.cs
@@ -23,22 +23,22 @@ namespace Identity_X_2024_v2.Controllers
         }
         //aby dodać rolę Admin należy uruchomić tą metodę
         //https://localhost:7070/permission/AddRole
-        public async Task<IActionResult> AddRole()
-        {
-            await _roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
-            _context.SaveChanges();
-            return View();
-        }
-        //aby przypisać rolę do użytkownika uruchamiamy poniższą metodę podając w linku email istniejącego w systmie użytkownika oraz nazwę roli
-        //https://localhost:7070/permission/AssignRole?email=email@email.pl&role=Admin
-        public async Task<IActionResult> AssignRole(string email, string role)
-        {
-            var user = await _userManager.FindByEmailAsync(email);
-            if (!await _userManager.IsInRoleAsync(user, role))
-            {
-                await _userManager.AddToRoleAsync(user, role);
-            }
-            return View();
-        }
+        //public async Task<IActionResult> AddRole()
+        //{
+        //    await _roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
+        //    _context.SaveChanges();
+        //    return View();
+        //}
+        ////aby przypisać rolę do użytkownika uruchamiamy poniższą metodę podając w linku email istniejącego w systmie użytkownika oraz nazwę roli
+        ////https://localhost:7070/permission/AssignRole?email=email@email.pl&role=Admin
+        //public async Task<IActionResult> AssignRole(string email, string role)
+        //{
+        //    var user = await _userManager.FindByEmailAsync(email);
+        //    if (!await _userManager.IsInRoleAsync(user, role))
+        //    {
+        //        await _userManager.AddToRoleAsync(user, role);
+        //    }
+        //    return View();
+        //}
     }
 }
