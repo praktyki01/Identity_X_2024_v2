603bb36d6493d98b13b5d384cea2e7e2dd78cfbc
ETI <ETI@DESKTOP-BEI752H>
2024-10-16 08:07:58 +0200

    Inicjalizacja bazy danych

---
+++ Identity_X_2024_v2/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Identity_X_2024_v2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+
+namespace Identity_X_2024_v2.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();           
+
+            //sprawdzamy czy są rekordy w bazie danych
+            if (context.Sport.Any())
+            {
+                return;   //są rekordy więc wychodzimy
+            }
+            var sporty = new Sport[]
+            {
+                new Sport{Nazwa="Bieganie",ImgUrl="https://"},
+                new Sport{Nazwa="Kolarstwo", ImgUrl="https://"},
+                new Sport{Nazwa="Pływanie",ImgUrl="https://"}
+            };
+            foreach (var sport in sporty)
+            {
+                context.Sport.Add(sport);
+            }
+            
+            context.SaveChanges();
+        }
+      
+    }
+}
--- Identity_X_2024_v2/Program.cs
+++ Identity_X_2024_v2/Program.cs
@@ -38,6 +38,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    DbInitializer.Initialize(context);
+}
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
